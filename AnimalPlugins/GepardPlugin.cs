0e9cf00afbb61f966b58383c110789527f07dac3
Domantė Žukauskaitė <61902997+zu-do@users.noreply.github.com>
2023-12-20 15:43:08 +0200

    Add files via upload

---
+++ AnimalPlugins/GepardPlugin.cs
@@ -0,0 +1,35 @@
+﻿using Savanna.Models;
+
+namespace AnimalPlugins
+{
+    public class GepardPlugin : IAnimalPlugin
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int VisionRange { get; set; }
+        public bool IsAlive { get; set; }
+        public bool IsNew { get; set; } = true;
+        public bool IsPredator { get; set; } = false;
+        public double Health { get; set; }
+        public char CharRepresentation { get; set; }
+
+        public GepardPlugin()
+        {
+            this.IsAlive = true;
+            this.IsPredator = true;
+            this.Health = 20;
+            this.CharRepresentation = 'G';
+            this.VisionRange = 2;
+        }
+
+        public override string ToString()
+        {
+            return "Gepard";
+        }
+
+        public void Die()
+        {
+            this.IsAlive = false;
+        }
+    }
+}
---
+++ AnimalPlugins/HippoPlugin.cs
@@ -0,0 +1,39 @@
+﻿using Savanna.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalPlugins
+{
+    public class HippoPlugin : IAnimalPlugin
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int VisionRange { get; set; }
+        public bool IsAlive { get; set; }
+        public bool IsNew { get; set; } = true;
+        public bool IsPredator { get; set; } = false;
+        public double Health { get; set; }
+        public char CharRepresentation { get; set; }
+
+        public HippoPlugin()
+        {
+            this.IsAlive = true;
+            this.Health = 20;
+            this.CharRepresentation = 'H';
+            this.VisionRange = 2;
+        }
+
+        public override string ToString()
+        {
+            return "Hippo";
+        }
+
+        public void Die()
+        {
+            this.IsAlive = false;
+        }
+    }
+}
