46f33215087984f809e195c86d11ee5d6b9554e9
Domantė Žukauskaitė <61902997+zu-do@users.noreply.github.com>
2023-12-20 15:43:54 +0200

    Add files via upload

---
+++ Savanna/Program.cs
@@ -0,0 +1,20 @@
+﻿using AnimalAttributes;
+using Savanna.Console;
+
+try
+{
+    PluginLoader loader = new PluginLoader();
+    loader.LoadPlugins(SavannaConstants.FolderName);
+}
+catch (Exception e)
+{
+    Console.WriteLine(string.Format("Plugins couldn't be loaded: {0}", e.Message));
+}
+
+
+IConsoleWrapper consoleWrapper = new ConsoleWrapper();
+IOutputHandler outputHandler = new OutputHandler(consoleWrapper);
+
+Game game = new Game(outputHandler);
+
+game.RunGame();
